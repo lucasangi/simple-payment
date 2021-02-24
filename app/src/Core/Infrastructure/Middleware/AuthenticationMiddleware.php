1c46aac544aa8e91423ecea637834b2c7d844116
Lucas Angeli <lucas.angeli@superlogica.com>
2021-02-23 21:30:01 -0300

    Configuração incial messenger e implementação do middleware de autenticação.

---
+++ app/src/Core/Infrastructure/Middleware/AuthenticationMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimplePayment\Core\Infrastructure\Middleware;
+
+use SimplePayment\Core\Domain\Authenticator;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+use Symfony\Component\Messenger\Middleware\StackInterface;
+
+class AuthenticationMiddleware implements MiddlewareInterface {
+
+    private Authenticator $authenticator;
+
+    public function __construct(Authenticator $authenticator)
+    {
+        $this->authenticator = $authenticator;
+    }
+
+    public function handle(Envelope $envelope, StackInterface $stack): Envelope {
+        $this->authenticator->authenticate();
+
+        return $envelope;
+    }
+
+} 
