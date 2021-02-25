9cef4d84dd17e950c46528328d13f36a381a4ee9
Lucas Angeli <lucas.angeli@superlogica.com>
2021-02-25 07:11:54 -0300

    Ajuste na migration.

--- app/migrations/Version20210223235948.php
+++ app/migrations/Version20210223235948.php
@@ -21,11 +21,13 @@ final class Version20210223235948 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE users (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', full_name VARCHAR(255) NOT NULL, cpf_or_cnpj VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, wallet_amount NUMERIC(8, 2) NOT NULL, type VARCHAR(255) NOT NULL, UNIQUE INDEX user_email (email), UNIQUE INDEX user_cpf_cpnj (cpf_or_cnpj), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME(6) NOT NULL, available_at DATETIME(6) NOT NULL, delivered_at DATETIME(6) DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
